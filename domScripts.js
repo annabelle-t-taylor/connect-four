4aaec8d417121ac81b54e06d926c55b5b7af8e84
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-04 15:58:01 -0500

    Saving what I have in order to try filling in board with Javascript; may want to revert

--- domScripts.js
+++ domScripts.js
@@ -1,13 +1,27 @@
 //note to self: refactor the e.compasedPath() into something that isn't quite so incidental
 
+//fall to lowest point possible
+
+
+
+
 const boardEl = document.querySelector('.board')
 const resetEl = document.querySelector('.reset')
 const cells = document.querySelectorAll('.cell')
 const nextPlayer = document.querySelector('.nextPlayer')
 let currentPlayer = 1
 
+// let boardState = [[],[],[],[],[],[]]
+
+// for (let i = 0; i < 7; i++){
+//     for (let j = 0; j < 6; j++){
+//         boardState[j].push(" ")
+//     }
+// }
+
 boardEl.addEventListener('click', function(e){
     if (e.composedPath().length === 7){
+        console.log(e)
         if (currentPlayer === 1){
             e.target.style.backgroundColor = 'red'
             e.target.style.pointerEvents = "none"
