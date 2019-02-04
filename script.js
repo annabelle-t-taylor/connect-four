7d02ff60f271f5fa18aea4f902e730824b13ecac
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-04 15:08:56 -0500

    Add Next Player information in variable

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const boardEl = document.querySelector('.board')
 const resetEl = document.querySelector('.reset')
 const cells = document.querySelectorAll('.cell')
+const nextPlayer = document.querySelector('.nextPlayer')
 let currentPlayer = 1
 
 boardEl.addEventListener('click', function(e){
@@ -10,11 +11,13 @@ boardEl.addEventListener('click', function(e){
         if (currentPlayer === 1){
             e.target.style.backgroundColor = 'red'
             e.target.style.pointerEvents = "none"
+            nextPlayer.innerText = "Black"
             currentPlayer = 2
         }
         else {
             e.target.style.backgroundColor = 'black'
             e.target.style.pointerEvents = "none"
+            nextPlayer.innerText = "Red"
             currentPlayer = 1
         }
     }
@@ -23,6 +26,7 @@ boardEl.addEventListener('click', function(e){
 resetEl.addEventListener('click', function(e){
     e.preventDefault()
     currentPlayer = 1
+    nextPlayer.innerText = "Red"
 
      for (i = 0; i < cells.length; i++){
          cells[i].style.backgroundColor = "white"
